4817619871c35623599fa1501c2730319a1859e3
krip5i <krip5i@krip5is-MacBook-Pro.local>
2025-04-11 06:16:48 +0300

    add npm-lock

--- src/entities/author/model/api.ts
+++ src/entities/author/model/api.ts
@@ -7,5 +7,8 @@ const authorUrl = urlJoin(API_URL, 'users');
 
 export const fetchAuthorById = async (authorId: string) => {
   const url = urlJoin(authorUrl, authorId);
-  return ky.get<Author>(url, { cache: 'no-store' }).json();
+  // it's good to use revalidate time cache for this request, but unfortunately kyjs doesn't support custom options
+  // for native fetch https://github.com/vercel/next.js/issues/48519#issuecomment-1587331280
+  // may be i'm can fix it later)
+  return ky.get<Author>(url).json();
 };
